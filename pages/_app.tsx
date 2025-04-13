06b4d0f5beb4b5c1b2797a2a78a74f2bced8ac1f
Edward_MIT <usmanoff19980404@gmail.com>
2025-04-13 15:45:55 +0900

    feat:SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { CssBaseline } from "@mui/material";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import{light} from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
